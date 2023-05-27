f346e283f005f37d20b7f7a0e88ef28cbf12545c
Antony Kimathi Kamencu <106080035+Bappi001@users.noreply.github.com>
2023-05-27 05:50:10 +0300

    Update builtin_emulators2.c

--- builtin_emulators2.c
+++ builtin_emulators2.c
@@ -3,7 +3,7 @@
 /**
  * _myhistory -  history list displayed, one command by line, preceded
  *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure which contains potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
@@ -14,7 +14,7 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to string
+ * unset_alias - Set alias to string
  * @info: parameter struct
  * @str: the string alias
  *
@@ -37,7 +37,7 @@ int unset_alias(info_t *info, char *str)
 }
 
 /**
- * set_alias - sets alias to string
+ * set_alias - Set alias to string
  * @info: parameter struct
  * @str: the string alias
  *
@@ -58,7 +58,7 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias string
+ * print_alias - alias string is printed
  * @node: the alias node
  *
  * Return: Always 0 on success, 1 on error
@@ -82,7 +82,7 @@ int print_alias(list_t *node)
 
 /**
  * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure which contains potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
