57c730c903ee11e141fcbee057c153165124cad0
Antony Kimathi Kamencu <106080035+Bappi001@users.noreply.github.com>
2023-05-27 05:56:37 +0300

    Update env2.c

--- env2.c
+++ env2.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
+ * get_environ - Return the string array copy of our environ
+ * @info: Structure which contains potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
@@ -18,8 +18,8 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
+ * _unsetenv - environment variable is removed
+ * @info: Structure which contains potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
@@ -50,7 +50,7 @@ int _unsetenv(info_t *info, char *var)
 }
 
 /**
- * _setenv - Initialize a new environment variable,
+ * _setenv - new environment variable is intialized,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
